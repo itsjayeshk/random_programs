e321c34ca074a42ede36a48c5bdf0231f4ff863c
itsjayeshk <jayeshkwal2209@gmail.com>
2025-07-19 13:30:28 +0530

    Delete FACTORIAL_ITERATIVE_VERSION.py

--- FACTORIAL_ITERATIVE_VERSION.py
+++
@@ -1,9 +0,0 @@
-def factorial_iterative(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-n = int(input("Enter the number whose factorial you want: "))
-print("Factorial (Recursive):", factorial(n))
-print("Factorial (Iterative):", factorial_iterative(n))
